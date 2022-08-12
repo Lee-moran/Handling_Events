0fee39497f0fd445663b968cc4bc9d1381ac1172
Lee-moran <leemoran06@hotmail.com>
2022-08-12 13:38:07 +0000

    parent to child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
-import EventBinding from "./components/EventsBinding";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+// import EventsClass from "./components/EventsClass";
+// import EventsFunctional from "./components/EventsFunctional";
+// import EventBinding from "./components/EventsBinding";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+// import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
   return <div className="App">
-      <EventsFunctional />
-      <EventsClass />
-      <EventBinding/>
+      {/* <EventsFunctional />
+      <EventsClass /> */}
+      {/* <EventBinding/>
       <ConditionalRenderingClass/>
       <ConditionalRenderingFunctional connected={true} />
+      <NestingComponents/> */}
+      <MethodsAsPropsParent />
+      
   </div>;
 }
 
